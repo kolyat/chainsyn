096236a881d2bef293d462ca803c632f66a2876e
Kolyat <kks.pub@gmail.com>
2016-07-23 21:43:28 +0300

    Add transcription() and unit-tests

--- processing.py
+++ processing.py
@@ -5,6 +5,7 @@
 Module "processing" with processing functions
 
 replication() - function of DNA replication
+transcription() - function of transcription from DNA to mRNA
 """
 
 
@@ -13,9 +14,9 @@ def replication(dna_chain):
     Function of DNA replication (DNA -> DNA)
 
     Arguments:
-        dna_chain -- string, list or tuple with nucleotides (A, T, C, G)
+        dna_chain -- string, list or tuple with DNA nucleotides (A, T, C, G)
 
-    Returns list with nucleotides of second DNA chain
+    Returns list of second DNA chain nucleotides
 
     Raises an exception if fails:
         - TypeError -- when dna_chain is not string, list or tuple;
@@ -24,10 +25,10 @@ def replication(dna_chain):
         - KeyError - when dna_chain contains not valid nucleotides
     """
     dna_pattern = {
-        'a': 't',   # Adenine associates with thymine (A-T)
-        't': 'a',   # Thymine associates with adenine (T-A)
-        'c': 'g',   # Cytosine associates with guanine (C-G)
-        'g': 'c'    # Guanine associates with cytosine (G-C)
+        'A': 'T',   # Adenine associates with thymine (A-T)
+        'T': 'A',   # Thymine associates with adenine (T-A)
+        'C': 'G',   # Cytosine associates with guanine (C-G)
+        'G': 'C'    # Guanine associates with cytosine (G-C)
     }
     # Check if dna_chain is correct type and not empty
     if type(dna_chain) not in (str, list, tuple):
@@ -38,7 +39,7 @@ def replication(dna_chain):
     dna1_chain = []
     for el in list(dna_chain):
         try:
-            dna1_chain.append(el.lower())
+            dna1_chain.append(el.upper())
         except ValueError:
             # dna_chain might contain non-alphabetic characters
             break
@@ -50,3 +51,47 @@ def replication(dna_chain):
         else:
             raise KeyError
     return dna2_chain
+
+
+def transcription(dna_chain):
+    """
+    Function of transcription (DNA -> mRNA)
+
+    Arguments:
+        dna_chain -- string, list or tuple with DNA nucleotides (A, T, C, G)
+
+    Returns list mRNA nucleotides
+
+    Raises an exception if fails:
+        - TypeError -- when dna_chain is not string, list or tuple;
+        - ValueError -- when dna_chain is empty or contains forbidden
+                        characters (non-alphabetic)
+        - KeyError - when dna_chain contains not valid nucleotides
+    """
+    mrna_pattern = {
+        'A': 'U',   # Adenine associates with uracil (A-U)
+        'T': 'A',   # Thymine associates with adenine (T-A)
+        'C': 'G',   # Cytosine associates with guanine (C-G)
+        'G': 'C'    # Guanine associates with cytosine (G-C)
+    }
+    # Check if dna_chain is correct type and not empty
+    if type(dna_chain) not in (str, list, tuple):
+        raise TypeError
+    if len(dna_chain) == 0:
+        raise ValueError
+    # Try to convert input dna_chain to list of nucleotides
+    dna = []
+    for el in list(dna_chain):
+        try:
+            dna.append(el.upper())
+        except ValueError:
+            # dna_chain might contain non-alphabetic characters
+            break
+    # Try to transcript mRNA
+    mrna = []
+    for n in dna:
+        if n in mrna_pattern:
+            mrna.append(mrna_pattern[n])
+        else:
+            raise KeyError
+    return mrna
--- test_processing.py
+++ test_processing.py
@@ -5,45 +5,47 @@
 Module "test_processing" contains unit-tests to processing module
 
 TestReplication - tests to replication()
+TestTranscription - tests to transcription()
 """
 import unittest
 
 from processing import replication
+from processing import transcription
 
 
 class TestReplication(unittest.TestCase):
     """
     Class TestReplication contains tests to replication()
     """
-    correct = ['t', 'a', 'g', 'c']
+    correct_dna = ['T', 'A', 'G', 'C']
 
     def test_correct_lower(self):
         """
         Positive test
         Correct string with nucleotides in lower case
         """
-        self.assertEqual(replication('atcg'), self.correct)
+        self.assertEqual(replication('atcg'), self.correct_dna)
 
     def test_correct_upper(self):
         """
         Positive test
         Correct string with nucleotides in upper case
         """
-        self.assertEqual(replication('ATCG'), self.correct)
+        self.assertEqual(replication('ATCG'), self.correct_dna)
 
     def test_correct_list(self):
         """
         Positive test
         Correct list of nucleotides
         """
-        self.assertEqual(replication(['a', 't', 'c', 'g']), self.correct)
+        self.assertEqual(replication(['a', 't', 'c', 'g']), self.correct_dna)
 
     def test_correct_tuple(self):
         """
         Positive test
         Correct tuple of nucleotides
         """
-        self.assertEqual(replication(('a', 't', 'c', 'g')), self.correct)
+        self.assertEqual(replication(('a', 't', 'c', 'g')), self.correct_dna)
 
     def test_empty(self):
         """
@@ -101,6 +103,98 @@ class TestReplication(unittest.TestCase):
         Not supported type - dictionary
         """
         self.assertRaises(TypeError, replication, {'a':'t', 'c':'g'})
+
+
+class TestTranscription(unittest.TestCase):
+    """
+    Class TestReplication contains tests to transcription()
+    """
+    correct_mrna = ['U', 'A', 'G', 'C']
+
+    def test_correct_lower(self):
+        """
+        Positive test
+        Correct string with nucleotides in lower case
+        """
+        self.assertEqual(transcription('atcg'), self.correct_mrna)
+
+    def test_correct_upper(self):
+        """
+        Positive test
+        Correct string with nucleotides in upper case
+        """
+        self.assertEqual(transcription('ATCG'), self.correct_mrna)
+
+    def test_correct_list(self):
+        """
+        Positive test
+        Correct list of nucleotides
+        """
+        self.assertEqual(transcription(['a', 't', 'c', 'g']), self.correct_mrna)
+
+    def test_correct_tuple(self):
+        """
+        Positive test
+        Correct tuple of nucleotides
+        """
+        self.assertEqual(transcription(('a', 't', 'c', 'g')), self.correct_mrna)
+
+    def test_empty(self):
+        """
+        Negative test
+        Empty string
+        """
+        self.assertRaises(ValueError, transcription, '')
+
+    def test_uracil(self):
+        """
+        Negative test
+        String with uracil (U)
+        """
+        self.assertRaises(KeyError, transcription, 'atcgu')
+
+    def test_forbidden_char(self):
+        """
+        Negative test
+        String with forbidden characters
+        """
+        self.assertRaises(KeyError, transcription, 'XYZat123cg#$%')
+
+    def test_forbidden_uracil_first(self):
+        """
+        Negative test
+        String with forbidden characters and uracil in first place
+        """
+        self.assertRaises(KeyError, transcription, 'uXYZat123cg#$%')
+
+    def test_forbidden_uracil_mid(self):
+        """
+        Negative test
+        String with forbidden characters and uracil
+        somewhere in the middle
+        """
+        self.assertRaises(KeyError, transcription, 'XuYZat123cg#$%')
+
+    def test_integer(self):
+        """
+        Negative test
+        Not a string (integer)
+        """
+        self.assertRaises(TypeError, transcription, 3)
+
+    def test_unicode(self):
+        """
+        Negative test
+        Unicode string with Russian 'АТС'
+        """
+        self.assertRaises(KeyError, transcription, 'АТС')
+
+    def test_dictionary(self):
+        """
+        Negative test
+        Not supported type - dictionary
+        """
+        self.assertRaises(TypeError, transcription, {'a':'t', 'c':'g'})
 
 if __name__ == '__main__':
     unittest.main()
