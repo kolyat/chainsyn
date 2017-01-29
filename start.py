51c9fe37b69385b76bfb5b4e7b9b5c14c80285fc
Kolyat <kks.pub@gmail.com>
2017-01-29 22:16:37 +0300

    Exception handling and code imrpovements

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Kirill 'Kolyat' Kiselnikov
+# Copyright (c) 2016-2017 Kirill 'Kolyat' Kiselnikov
 # This file is the part of chainsyn, released under modified MIT license
 # See the file LICENSE.txt included in this distribution
 
@@ -16,133 +16,9 @@ import configparser
 from uniprocessing import *
 
 
-def selection_mode(screen):
-    """
-    Switch to selection mode (e. g. menu item selection)
-
-    :param screen: curses stdscr object
-
-    :raise OSError: when curses stdscr object is not defined
-    """
-
-    if not screen:
-        raise OSError('Stdscr object is not defined')
-    curses.noecho()
-    curses.cbreak()
-    screen.keypad(True)
-
-
-def input_mode(screen):
-    """
-    Switch to input mode (e. g. to write string of nucleotides)
-
-    :param screen: curses stdscr object
-
-    :raise OSError: when curses stdscr object is not defined
-    """
-
-    if not screen:
-        raise OSError('Stdscr object is not defined')
-    curses.echo()
-    curses.nocbreak()
-    screen.keypad(False)
-
-
-def print_results(screen, *chains):
-    """
-    Print results of genetic processes
-
-    :param screen: curses stdscr object
-    :param chains: set of DNA/RNA/amino acid chains, their number should be
-                   equal to 4 or 5
-
-    :raise OSError: when curses stdscr object is not defined
-    :raise TypeError: if chain is not a list
-    :raise ValueError: when number of chains is not 4 or 5, if number of items
-                       in chains is not equal
-    """
-
-    # Necessary checks
-    if not screen:
-        raise OSError('stdscr object is not defined')
-    if len(chains) < 4 or len(chains) > 5:
-        raise ValueError('Number of arguments must be 4 or 5, '
-                         'current - {}'.format(len(chains)))
-    for i, c in enumerate(chains, start=0):
-        if type(c) != list:
-            raise TypeError('Argument {} should be a list, not {}'.format(
-                i+1, type(c)))
-        if i < len(chains)-1:
-            if len(c) != len(chains[i+1]):
-                raise ValueError(
-                    'Number of items in arguments {} and {} are not equal: '
-                    '{} != {}'.format(i+1, i+2, len(c), len(chains[i+1]))
-                )
-
-    screen.clear()
-    # Check of terminal supports colors
-    if curses.has_colors():
-        # Set up dark gray if possible
-        if curses.can_change_color():
-            curses.init_color(curses.COLOR_WHITE, 70, 70, 70)
-        # Init color pairs
-        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-        curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        # Print color legend
-        screen.addstr('Amino acids:\n')
-        screen.addstr('nonpolar   ', curses.color_pair(1))
-        screen.addstr('polar   ', curses.color_pair(2))
-        screen.addstr('basic   ', curses.color_pair(3))
-        screen.addstr('acidic   ', curses.color_pair(4))
-        screen.addstr('(stop codon)\n\n\n', curses.color_pair(5))
-        screen.refresh()
-        # Init color pattern for amino acids
-        color_pattern = {
-            'Phe': curses.color_pair(1),
-            'Leu': curses.color_pair(1),
-            'Ser': curses.color_pair(2),
-            'Pro': curses.color_pair(1),
-            'His': curses.color_pair(3),
-            'Gln': curses.color_pair(2),
-            'Tyr': curses.color_pair(2),
-            'xxx': curses.color_pair(5),
-            'Cys': curses.color_pair(2),
-            'Trp': curses.color_pair(1),
-            'Arg': curses.color_pair(3),
-            'Ile': curses.color_pair(1),
-            'Met': curses.color_pair(1),
-            'Thr': curses.color_pair(2),
-            'Asn': curses.color_pair(2),
-            'Lys': curses.color_pair(3),
-            'Val': curses.color_pair(1),
-            'Ala': curses.color_pair(1),
-            'Asp': curses.color_pair(4),
-            'Glu': curses.color_pair(4),
-            'Gly': curses.color_pair(1)
-        }
-
-    # Print items in chains one by one
-    for i in range(len(chains[0])):
-        for n, c in enumerate(chains, start=0):
-            if curses.has_colors() and c[i] in color_pattern:
-                screen.addstr(c[i], color_pattern[c[i]])
-            else:
-                screen.addstr(c[i])
-            if n == len(chains)-1:
-                screen.addstr('\n')
-                screen.refresh()
-                time.sleep(0.2)
-            else:
-                screen.addstr('-')
-                screen.refresh()
-                time.sleep(0.1)
-
-    # Print conclusion
-    screen.addstr('\n')
-    screen.addstr('Processed {} codon(s)\n'.format(len(chains[0])))
+class RoutineErr(Exception):
+    """Exception class for module's routines (e. g. file IO)"""
+    pass
 
 
 def to_file(exp_dir, *chains):
@@ -153,25 +29,27 @@ def to_file(exp_dir, *chains):
     :param chains: set of DNA/RNA/amino acid chains, their number should be
                    equal to 4 or 5
 
-    :raise OSError: if cannot create output file
-    :raise TypeError: if chain is not a list
-    :raise ValueError: when number of chains is not 4 or 5, if number of items
-                       in chains is not equal
+    :raise RoutineErr:
+      - if cannot create output file
+      - chain is not list
+      - number of chains is not 4 or 5
+      - number of chain's items is not equal to each other
     """
 
     # Necessary checks
     if len(chains) < 4 or len(chains) > 5:
-        raise ValueError('Number of arguments must be 4 or 5, '
-                         'current - {}'.format(len(chains)))
+        raise RoutineErr('{}: number of arguments must be 4 or 5, '
+                         'got {}'.format(to_file.__name__, len(chains)))
     for i, c in enumerate(chains, start=0):
         if type(c) != list:
-            raise TypeError('Argument {} should be a list, not {}'.format(
-                i+1, type(c)))
+            raise RoutineErr('{}: argument {} should be a list, not {}'.format(
+                to_file.__name__, i+1, type(c)))
         if i < len(chains)-1:
             if len(c) != len(chains[i+1]):
-                raise ValueError(
-                    'Number of items in arguments {} and {} are not equal: '
-                    '{} != {}'.format(i+1, i+2, len(c), len(chains[i+1]))
+                raise RoutineErr(
+                    '{}: number of items in arguments {} and {} '
+                    'are not equal: {} != {}'.format(
+                        to_file.__name__, i+1, i+2, len(c), len(chains[i+1]))
                 )
 
     # Pick current date and time
@@ -181,9 +59,8 @@ def to_file(exp_dir, *chains):
     try:
         out = open(os.path.join(exp_dir, 'chains-{}.txt'.format(now)), 'wt')
     except OSError:
-        print('Could not open file: {}'.format(
-            os.path.join(exp_dir, 'chains-{}.txt'.format(now))))
-        return None
+        raise RoutineErr('{}: could not open file: {}'.format(to_file.__name__,
+                         os.path.join(exp_dir, 'chains-{}.txt'.format(now))))
     # Write data to file
     for i in range(len(chains[0])):
         for n, c in enumerate(chains, start=0):
@@ -221,14 +98,15 @@ def from_file(source_file):
     :type: str
 
     :return: str with nucleotides
+    :raise RoutineErr if cannot open source file
     """
 
     # Try to open file
     try:
         f = open(r'{}'.format(source_file), 'r')
     except OSError:
-        print('Cannot open file {}'.format(source_file))
-        return None
+        raise RoutineErr('{}: cannot open file {}'.format(from_file.__name__,
+                                                          source_file))
 
     ignore_sym = (' ', '\n')  # Symbols to be ignored
     raw_data = []
@@ -248,11 +126,121 @@ def main(screen):
     """
     Main function
 
-    :param screen: curses stdscr object
+    :param screen: main window
     """
 
+    def selection_mode():
+        """Switch to selection mode (e. g. menu item selection)"""
+        curses.noecho()
+        curses.cbreak()
+        screen.keypad(True)
+
+    def input_mode():
+        """Switch to input mode (e. g. to write string of nucleotides)"""
+        curses.echo()
+        curses.nocbreak()
+        screen.keypad(False)
+
+    def print_results(*chains):
+        """
+        Print results of genetic processes
+
+        :param chains: set of DNA/RNA/amino acid chains, their number should be
+                       equal to 4 or 5
+
+        :raise RoutineErr:
+          - chain is not a list
+          - number of chains is not 4 or 5
+          - number of items in chains is not equal
+        """
+
+        # Necessary checks
+        if len(chains) < 4 or len(chains) > 5:
+            raise RoutineErr('{}: number of arguments must be 4 or 5, '
+                             'got {}'.format(print_results.__name__,
+                                             len(chains)))
+        for i, c in enumerate(chains, start=0):
+            if type(c) != list:
+                raise RoutineErr('{}: argument {} should be a list, '
+                                 'not {}'.format(print_results.__name__,
+                                                 i + 1, type(c)))
+            if i < len(chains) - 1:
+                if len(c) != len(chains[i + 1]):
+                    raise RoutineErr(
+                        '{}: number of items in arguments {} and {} '
+                        'are not equal: {} != {}'.format(
+                            print_results.__name__, i + 1, i + 2,
+                            len(c), len(chains[i + 1]))
+                    )
+
+        screen.clear()
+        # Check of terminal supports colors
+        if curses.has_colors():
+            # Set up dark gray if possible
+            if curses.can_change_color():
+                curses.init_color(curses.COLOR_WHITE, 70, 70, 70)
+            # Init color pairs
+            curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+            curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+            curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
+            curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+            curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)
+            # Print color legend
+            screen.addstr('Amino acids:\n')
+            screen.addstr('nonpolar   ', curses.color_pair(1))
+            screen.addstr('polar   ', curses.color_pair(2))
+            screen.addstr('basic   ', curses.color_pair(3))
+            screen.addstr('acidic   ', curses.color_pair(4))
+            screen.addstr('(stop codon)\n\n\n', curses.color_pair(5))
+            screen.refresh()
+            # Init color pattern for amino acids
+            color_pattern = {
+                'Phe': curses.color_pair(1),
+                'Leu': curses.color_pair(1),
+                'Ser': curses.color_pair(2),
+                'Pro': curses.color_pair(1),
+                'His': curses.color_pair(3),
+                'Gln': curses.color_pair(2),
+                'Tyr': curses.color_pair(2),
+                'xxx': curses.color_pair(5),
+                'Cys': curses.color_pair(2),
+                'Trp': curses.color_pair(1),
+                'Arg': curses.color_pair(3),
+                'Ile': curses.color_pair(1),
+                'Met': curses.color_pair(1),
+                'Thr': curses.color_pair(2),
+                'Asn': curses.color_pair(2),
+                'Lys': curses.color_pair(3),
+                'Val': curses.color_pair(1),
+                'Ala': curses.color_pair(1),
+                'Asp': curses.color_pair(4),
+                'Glu': curses.color_pair(4),
+                'Gly': curses.color_pair(1)
+            }
+
+        # Print items in chains one by one
+        for i in range(len(chains[0])):
+            for n, c in enumerate(chains, start=0):
+                if curses.has_colors() and c[i] in color_pattern:
+                    screen.addstr(c[i], color_pattern[c[i]])
+                else:
+                    screen.addstr(c[i])
+                if n == len(chains) - 1:
+                    screen.addstr('\n')
+                    screen.refresh()
+                    time.sleep(0.2)
+                else:
+                    screen.addstr('-')
+                    screen.refresh()
+                    time.sleep(0.1)
+
+        # Print conclusion
+        screen.addstr('\n')
+        screen.addstr('Processed {} codon(s)\n'.format(len(chains[0])))
+
+    # Init main window
     screen.scrollok(True)
-    selection_mode(screen)
+    selection_mode()
     screen.clear()
 
     # Set up settings
@@ -300,67 +288,107 @@ def main(screen):
         screen.addstr('\n')
         screen.refresh()
         item = screen.getkey()
+    screen.clear()
 
     # Eucariotic cell polypeptide synthesis
     if item == menu_items[0]:
-        screen.clear()
         screen.addstr('Enter source DNA chain with nucleotides A, T, C or G\n')
         screen.addstr('(or path to source file)\n')
         screen.addstr('> ')
         screen.refresh()
-        input_mode(screen)
+        input_mode()
         y, x = screen.getyx()
         raw_string = screen.getstr(y, x)
         raw_data = str(raw_string)[2:-1]
+        dna1 = list()
+        dna2 = list()
+        mrna = list()
+        polypeptide = list()
         if is_file(raw_data):
-            raw_dna = from_file(raw_data)
-            dna1 = slice_chain(raw_dna.upper())
+            raw_dna = ''
+            try:
+                raw_dna = from_file(raw_data)
+            except RoutineErr as err:
+                screen.addstr(err)
+            try:
+                dna1 = slice_chain(raw_dna.upper())
+            except ProcessErr as err:
+                screen.addstr(err)
         else:
-            dna1 = slice_chain(raw_data.upper())
+            try:
+                dna1 = slice_chain(raw_data.upper())
+            except ProcessErr as err:
+                screen.addstr(err)
         # Process input chain
-        dna2 = process(dna1, pattern_dna)
-        mrna = process(dna2, pattern_mrna)
-        polypeptide = translation(mrna)
+        try:
+            dna2 = process(dna1, pattern_dna)
+            mrna = process(dna2, pattern_mrna)
+            polypeptide = translation(mrna)
+        except ProcessErr as err:
+            screen.addstr(err)
         # Print results
-        selection_mode(screen)
-        print_results(screen, dna1, dna2, mrna, polypeptide)
+        selection_mode()
+        print_results(dna1, dna2, mrna, polypeptide)
         # Export to text file
         if settings.has_section('EXPORT') and \
                 settings.has_option('EXPORT', 'Export'):
             if settings.getboolean('EXPORT', 'Export'):
-                to_file(settings['EXPORT']['ExportDir'],
-                        dna1, dna2, mrna, polypeptide)
+                try:
+                    to_file(settings['EXPORT']['ExportDir'],
+                            dna1, dna2, mrna, polypeptide)
+                except RoutineErr as err:
+                    screen.addstr(err)
 
     # Imitate full virus cycle
     if item == menu_items[1]:
-        screen.clear()
         screen.addstr('Enter viral mRNA chain with nucleotides A, U, C or G\n')
         screen.addstr('(or path to source file)\n')
         screen.addstr('> ')
         screen.refresh()
-        input_mode(screen)
+        input_mode()
         y, x = screen.getyx()
         raw_string = screen.getstr(y, x)
         raw_data = str(raw_string)[2:-1]
+        mrna1 = list()
+        dna1 = list()
+        dna2 = list()
+        mrna2 = list()
+        polypeptide = list()
         if is_file(raw_data):
-            raw_mrna = from_file(raw_data)
-            mrna1 = slice_chain(raw_mrna.upper())
+            raw_mrna = ''
+            try:
+                raw_mrna = from_file(raw_data)
+            except RoutineErr as err:
+                screen.addstr(err)
+            try:
+                mrna1 = slice_chain(raw_mrna.upper())
+            except ProcessErr as err:
+                screen.addstr(err)
         else:
-            mrna1 = slice_chain(raw_data.upper())
+            try:
+                mrna1 = slice_chain(raw_data.upper())
+            except ProcessErr as err:
+                screen.addstr(err)
         # Process input mRNA
-        dna1 = process(mrna1, pattern_dna_rev)
-        dna2 = process(dna1, pattern_dna)
-        mrna2 = process(dna2, pattern_mrna)
-        polypeptide = translation(mrna2)
+        try:
+            dna1 = process(mrna1, pattern_dna_rev)
+            dna2 = process(dna1, pattern_dna)
+            mrna2 = process(dna2, pattern_mrna)
+            polypeptide = translation(mrna2)
+        except ProcessErr as err:
+            screen.addstr(err)
         # Print results
-        selection_mode(screen)
-        print_results(screen, mrna1, dna1, dna2, mrna2, polypeptide)
+        selection_mode()
+        print_results(mrna1, dna1, dna2, mrna2, polypeptide)
         # Export to text file
         if settings.has_section('EXPORT') and \
                 settings.has_option('EXPORT', 'Export'):
             if settings.getboolean('EXPORT', 'Export'):
-                to_file(settings['EXPORT']['ExportDir'],
-                        mrna1, dna1, dna2, mrna2, polypeptide)
+                try:
+                    to_file(settings['EXPORT']['ExportDir'],
+                            mrna1, dna1, dna2, mrna2, polypeptide)
+                except RoutineErr as err:
+                    screen.addstr(err)
 
     # Exit
     if item == menu_items[2]:
@@ -369,8 +397,9 @@ def main(screen):
     screen.addstr('Press any key to exit')
     screen.refresh()
     screen.getkey()
-    input_mode(screen)
+    input_mode()
     curses.endwin()
+
 
 if __name__ == '__main__':
     curses.wrapper(main)
--- uniprocessing.py
+++ uniprocessing.py
@@ -1,32 +1,29 @@
-# Copyright (c) 2016 Kirill 'Kolyat' Kiselnikov
+# Copyright (c) 2016-2017 Kirill 'Kolyat' Kiselnikov
 # This file is the part of chainsyn, released under modified MIT license
 # See the file LICENSE.txt included in this distribution
 
 """
-Module "uniprocessing" with patterns and universal processing function
-
-process() - function that process one chain to another using given pattern
-
-Valid patterns:
-pattern_dna - to replicate another DNA chain
-pattern_mrna - to transcript mRNA from DNA
-pattern_dna_rev - to transcript DNA from mRNA (reverted transcription)
+Module with newer processing functions
 """
 
 
-pattern_dna = {
+class ProcessErr(Exception):
+    """Exception class for process errors"""
+    pass
+
+pattern_dna = {  # DNA to DNA
     'A': 'T',  # Adenine - Thymine
     'T': 'A',  # Thymine - Adenine
     'C': 'G',  # Cytosine - Guanine
     'G': 'C'   # Guanine - Cytosine
 }
-pattern_mrna = {
+pattern_mrna = {  # DNA to mRNA
     'A': 'U',  # Adenine - Uracil
     'T': 'A',  # Thymine - Adenine
     'C': 'G',  # Cytosine - Guanine
     'G': 'C'   # Guanine - Cytosine
 }
-pattern_dna_rev = {
+pattern_dna_rev = {  # mRNA to DNA
     'A': 'T',  # Adenine - Thymine
     'U': 'A',  # Uracil - Adenine
     'C': 'G',  # Cytosine - Guanine
@@ -38,25 +35,30 @@ valid_patterns = (pattern_dna, pattern_mrna, pattern_dna_rev)
 
 def slice_chain(chain):
     """
-    Function that slices input chain into codons
+    Slice input chain into codons
 
     :param chain: input chain of nucleotides, must be divisible by 3
     :type chain: str
 
     :return list of codons (strings)
 
-    :raise TypeError: when input chain is not string
-    :raise ValueError: if chain is empty or chain's length is not divisible
-                        by 3
+    :raise ProcessErr:
+      - input chain is not string
+      - chain is empty
+      - chain's length is not divisible by 3
     """
 
     # Necessary checks
     if type(chain) != str:
-        raise TypeError('Type of input chain must be string, list or tuple')
+        raise ProcessErr('{}: type of input chain must be str, '
+                         'got {}'.format(slice_chain.__name__, type(chain)))
     if not chain:
-        raise ValueError('Input chain is empty')
+        raise ProcessErr('{}: input chain is '
+                         'empty'.format(slice_chain.__name__))
     if len(chain) % 3 != 0:
-        raise ValueError('Length of chain must be divisible by 3')
+        raise ProcessErr('{}: length of chain must be divisible by 3, '
+                         'got {} nucleotides'.format(slice_chain.__name__,
+                                                     len(chain)))
 
     raw_chain = list(chain)
     output_chain = []
@@ -72,49 +74,60 @@ def process(chain, pattern):
     """
     Function of universal processing
 
-    :parameter chain: codons (strings) with nucleotides
+    :param chain: codons (strings) with nucleotides
     :type chain: list
 
-    :parameter pattern: any pattern (dictionary) which is used to build another
+    :param pattern: any pattern (dictionary) which is used to build another
                         chain
-    :type pattern: dictionary
+    :type pattern: dict
 
-    :returns list with codons (strings) of second chain
+    :return list with codons (strings) of second chain
 
-    :raises TypeError: when chain is not list, codon is not a string, pattern
-                       is not a dictionary;
-    :raises ValueError: when chain or pattern is empty, number of nucleotides
-                        is not equal to 3;
-    :raises KeyError: when codon contains not correct nucleotides or pattern is
-                      not valid;
+    :raise ProcessErr:
+      - chain is not list
+      - codon is not a string
+      - pattern is not a dictionary
+      - pattern is empty
+      - chain is empty
+      - number of chain's nucleotides is not equal to 3
+      - codon contains invalid nucleotides
+      - pattern is not valid
     """
+
     # Check if input chain is correct type and not empty
     if type(chain) != list:
-        raise TypeError('Input chain must be list of codons')
+        raise ProcessErr('{}: input chain must be list of codons, '
+                         'got {}'.format(process.__name__, type(chain)))
     if not chain:
-        raise ValueError('Input chain is empty')
+        raise ProcessErr('{}: input chain is empty')
     # Check if input pattern is correct type and valid
     if type(pattern) != dict:
-        raise TypeError('Input pattern must be dictionary type')
+        raise ProcessErr('{}: input pattern must be dictionary type, '
+                         'got {}'.format(process.__name__, type(pattern)))
     if not pattern:
-        raise ValueError('Input pattern is empty')
+        raise ProcessErr('{}: input pattern is empty'.format(process.__name__))
     if pattern not in valid_patterns:
-        raise KeyError('Input pattern is not valid')
+        raise ProcessErr('{}: input pattern is not '
+                         'valid'.format(process.__name__))
     # Check every codon of input chain
     for c in range(len(chain)):
         if type(chain[c]) != str:
-            raise TypeError('Error in codon {}: codon must be '
-                            'string, not {}'.format(c+1, type(chain[c])))
+            raise ProcessErr('{}: error in codon {}: '
+                             'codon must be string, '
+                             'not {}'.format(process.__name__,
+                                             c+1, type(chain[c])))
         if len(chain[c]) != 3:
-            raise ValueError('Error in codon {}: number of '
-                             'nucleotides equal to {}, '
-                             'must be 3'.format(c+1, len(chain[c])))
+            raise ProcessErr('{}: error in codon {}: '
+                             'number of nucleotides equal to {}, '
+                             'must be 3'.format(process.__name__,
+                                                c+1, len(chain[c])))
         for n in range(len(chain[c])):
             if chain[c][n].upper() not in pattern:
-                raise KeyError(
-                    'Error in codon {}, '
+                raise ProcessErr(
+                    '{}: error in codon {}, '
                     'nucleotide {}: '
-                    'unexpected nucleotide - '.format(c+1, n+1, chain[c][n]))
+                    'unexpected nucleotide - '.format(process.__name__,
+                                                      c+1, n+1, chain[c][n]))
 
     # Process input chain
     processed_chain = []
@@ -123,7 +136,6 @@ def process(chain, pattern):
         for n in c:
             codon += pattern[n.upper()]
         processed_chain.append(codon)
-
     return processed_chain
 
 
@@ -136,10 +148,12 @@ def translation(mrna_chain):
 
     :return list of polypeptide chain
 
-    :raise TypeError: when mrna_chain is not list, codon is not string;
-    :raise ValueError: when mrna_chain is empty, number of nucleotides is not
-                        equal to 3
-    :raise KeyError: when mrna_chain contains not valid nucleotides
+    :raise ProcessErr:
+      - mrna_chain is not list
+      - codon is not str
+      - mrna_chain is empty
+      - number of nucleotides is not equal to 3
+      - mrna_chain contains not valid nucleotides
     """
 
     mrna_nucleotides = ['A', 'U', 'C', 'G']
@@ -233,24 +247,30 @@ def translation(mrna_chain):
 
     # Check if mrna_chain is correct type and not empty
     if type(mrna_chain) != list:
-        raise TypeError('Input mRNA chain must be list of codons')
-    if len(mrna_chain) == 0:
-        raise ValueError('Input mRNA chain is empty')
+        raise ProcessErr('{}: input mRNA chain must be list of codons, '
+                         'got {}'.format(translation.__name__,
+                                         type(mrna_chain)))
+    if not mrna_chain:
+        raise ProcessErr('{}: input mRNA chain '
+                         'is empty'.format(translation.__name__))
     # Check every codon
     for c in range(len(mrna_chain)):
         if type(mrna_chain[c]) != str:
-            raise TypeError('Error in codon {}: codon must be '
-                            'string, not {}'.format(c+1, type(mrna_chain[c])))
+            raise ProcessErr('{}: error in codon {}: codon must be string, '
+                             'not {}'.format(translation.__name__,
+                                             c+1, type(mrna_chain[c])))
         if len(mrna_chain[c]) != 3:
-            raise ValueError('Error in codon {}: number of '
-                             'nucleotides equal to {}, '
-                             'must be 3'.format(c+1, len(mrna_chain[c])))
+            raise ProcessErr('{}: error in codon {}: number of nucleotides '
+                             'equal to {}, '
+                             'must be 3'.format(translation.__name__,
+                                                c+1, len(mrna_chain[c])))
         for n in range(len(mrna_chain[c])):
             if mrna_chain[c][n].upper() not in mrna_nucleotides:
-                raise KeyError(
-                    'Error in codon {}, '
+                raise ProcessErr(
+                    '{}: error in codon {}, '
                     'nucleotide {}: '
-                    'unexpected nucleotide - {}'.format(c+1, n+1,
+                    'unexpected nucleotide - {}'.format(translation.__name__,
+                                                        c+1, n+1,
                                                         mrna_chain[c][n]))
 
     # Translate mRNA to polypeptide chain
