055f3c5c62bc5538e58242907bf16d2eeb40bb06
kolyat <kks.pub@gmail.com>
2017-11-23 03:06:01 +0300

    Implement input data processing

--- web/views.py
+++ web/views.py
@@ -4,9 +4,27 @@
 
 import flask
 from web import web_interface, forms
+from core import processing
 
 
 @web_interface.route('/', methods=['GET', 'POST'])
 def main():
     editor_form = forms.EditorForm()
-    return flask.render_template('main.html', editor_form=editor_form)
+    editor_form.mode.data = 'replication'
+    if flask.request.method == 'GET':
+        output = ''
+    else:
+        if editor_form.validate_on_submit():
+            chain = processing.Chain('', editor_form.input_area.data)
+            if editor_form.mode.data == 'replication':
+                output = chain.replicate()
+            elif editor_form.mode.data == 'transcription':
+                output = chain.transcribe()
+            elif editor_form.mode.data == 'translation':
+                output = chain.translate()
+            else:
+                output = ''
+        else:
+            output = ''
+    return flask.render_template('main.html', editor_form=editor_form,
+                                 output=output)
